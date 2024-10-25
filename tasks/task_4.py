9d6a2a01d4160d815b9ca7da80e09d243f5a9ea7
Kiri <52772043+Melon-peel@users.noreply.github.com>
2024-10-25 04:00:33 +0300

    Added files to comply with repo structure

---
+++ tasks/task_4.py
@@ -0,0 +1,9 @@
+companions = ["Astarion", "Gale", "Karlach", "Lae'zel", "Shadowheart", "Wyll", "The Dark Urge", "Halsin", "Jaheira", "Minsc", "Minthara", "Alfira", "Losiir"]
+
+# Your code here ( • ᴗ - ) ✧
+
+print(companions[1:4])
+print(companions[1:14:2])
+print(companions[-2:-13:-3])
+print(companions[0::12])
+
